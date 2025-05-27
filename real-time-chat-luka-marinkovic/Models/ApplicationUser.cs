11f70b77a49d46f4eea394d1ccb61ea71010777e
LukaMarinkovic220 <lmarinkovic220s@gmail.com>
2025-05-27 14:02:59 +0200

    Dodatno polje za korisnika i model message

---
+++ real-time-chat-luka-marinkovic/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace real_time_chat_luka_marinkovic.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? DisplayName { get; set; }
+    }
+}
---
+++ real-time-chat-luka-marinkovic/Models/Message.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace real_time_chat_luka_marinkovic.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string SenderId { get; set; }
+
+        [Required]
+        public string ReceiverId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("SenderId")]
+        public ApplicationUser Sender { get; set; }
+
+        [ForeignKey("ReceiverId")]
+        public ApplicationUser Receiver { get; set; }
+    }
+}
