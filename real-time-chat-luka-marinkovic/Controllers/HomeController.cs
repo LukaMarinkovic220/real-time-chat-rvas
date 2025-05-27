3ad5ef1a1651dfc32464108a3bfaaeec0dc41303
LukaMarinkovic220 <lmarinkovic220s@gmail.com>
2025-05-27 13:44:33 +0200

    initial

---
+++ real-time-chat-luka-marinkovic/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using real_time_chat_luka_marinkovic.Models;
+
+namespace real_time_chat_luka_marinkovic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ real-time-chat-luka-marinkovic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace real_time_chat_luka_marinkovic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
